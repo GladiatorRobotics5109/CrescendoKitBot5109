8f6a803b9dc54de53029fe1e3c2b204d34d50b89
Nikhil J <nikhiljammal@gmail.com>
2024-02-06 15:53:35 -0500

    update to shooter class

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,10 +1,19 @@
-// package frc.robot.subsystems;
+ package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+ import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
+ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 
-// public class Shooter extends SubsystemBase{
-//     private final CAN
-// }
+ public class Shooter extends SubsystemBase{
+      private final CANSparkMax m_controller;
+      
+      public Shooter() {
+        m_controller = new CANSparkMax(0, null) 
+
+      }
+
+
+ }
