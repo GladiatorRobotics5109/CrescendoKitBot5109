ad4272b4afa3cbd956aa6321c18a32dea46b5eaa
Nikhil J <nikhiljammal@gmail.com>
2024-02-07 16:09:45 -0500

    fixing another error

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,17 +4,14 @@
 
 package frc.robot;
 
-import javax.sound.midi.Soundbank;
+// import javax.sound.midi.Soundbank;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-<<<<<<< HEAD
-=======
-import frc.robot.subsystems.Shoot;
 import edu.wpi.first.wpilibj.XboxController;
 
->>>>>>> cc335218bfe25312225cf7eff6f3ada050492642
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -92,26 +89,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-<<<<<<< HEAD
-    
-=======
-
-    //motors.m_shooter.set(1);
-    
-    
-    boolean shootButtonPressed = m_controller.getRawButtonPressed(0);
-
-    if (shootButtonPressed) {
-
-      motors.m_shooter.set(1);
-      System.out.println("Button pressed");
       
-    }else{
-      motors.m_shooter.set(0);
-      System.out.println("Button not pressed");
-
-    }
->>>>>>> cc335218bfe25312225cf7eff6f3ada050492642
   }
 
   @Override
