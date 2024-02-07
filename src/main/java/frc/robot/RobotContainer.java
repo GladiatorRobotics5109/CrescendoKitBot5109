d414d0b6f6d8fe015a13ab4fc5c9b95a7af7fd4a
Nikhil J <nikhiljammal@gmail.com>
2024-02-07 15:53:58 -0500

    bind shooter and feeder to controller triggers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,10 +8,12 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
+ 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -21,6 +23,10 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  private final Shooter m_shooterSubsystem = new Shooter();
+  // private final Shooter m_feeder = new Shooter();
+
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -43,12 +49,15 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    m_driverController.leftTrigger().whileTrue(m_shooterSubsystem.startShooter());
+    m_driverController.rightTrigger().whileTrue(m_shooterSubsystem.startFeed());
+
+
   }
 
   /**
@@ -59,5 +68,10 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     return Autos.exampleAuto(m_exampleSubsystem);
+  }
+
+  public void teleopPeriodic(){
+
+    
   }
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,26 +1,31 @@
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
  public class Shooter extends SubsystemBase{
       private final CANSparkMax m_feeder;
-      private final CANSparkMax m_shooter; 
+      private final CANSparkMax m_shooter;
       private double m_voltageShooter;
       private double m_voltageFeeder;
-
       
       public Shooter() {
         //m_controller = new CANSparkMax(18, MotorType.kBrushed);
         m_feeder = new CANSparkMax(18, MotorType.kBrushed);
         m_shooter = new CANSparkMax(3, MotorType.kBrushed);
-        m_voltageShooter = 1;
-        m_voltageFeeder = 1;
+        m_voltageShooter = 0;
+        m_voltageFeeder = 0;
         m_shooter.set(m_voltageShooter);
         m_feeder.set(m_voltageFeeder);
        
       }
+      public Command startShooter() {
+        return this.runOnce(() -> m_shooter.set(1));
 
-
+      }
+      public Command startFeed() {
+        return this.runOnce(() -> m_feeder.set(1));
+      }
  }
