3856400fe11474bd081402b0f9a3ccfaaa9df0b1
Nikhil J <nikhiljammal@gmail.com>
2024-02-21 17:20:41 -0500

    make can spark for intake

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -1,36 +1,44 @@
 package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase{
-    private final Talon m_intake;
+    private final Talon m_BottomIntake;
+    private final CANSparkMax m_TopIntake;
     private double m_voltageIntake;
     boolean take;
 
+
     public Intake(){
-        m_intake = new Talon(8);
+        m_BottomIntake = new Talon(8);
+        m_TopIntake = new CANSparkMax(14, MotorType.kBrushless);
         m_voltageIntake = 0;
-        m_intake.set(m_voltageIntake);
+        m_BottomIntake.set(m_voltageIntake);
+        m_TopIntake.set(m_voltageIntake);
     }
     //public Command startIntake(){
      //   return this.runOnce(() -> {
-       //     m_intake.set(1);
+       //     m_BottomIntake.set(1);
          //   System.out.println("balls");
           //});
     //}
     //public Command stopIntake(){
       //  return this.runOnce(() -> {
-        //    m_intake.set(0);
+        //    m_BottomIntake.set(0);
           //  System.out.println("balls 2.0 ");
           //});
     //}
     public Command toggleTake() {
         return this.runOnce(() -> {if (take == false){
-          m_intake.set(1);
+          m_BottomIntake.set(1);
+          m_TopIntake.set(1);
           take = true; 
         }else{
-          m_intake.set(0);
+          m_BottomIntake.set(0);
+          m_TopIntake.set(0);
           take = false;
         }
         });
